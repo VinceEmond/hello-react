1058f449968ab0df03506645abacbcab1816fe72
Vince Emond <vince.emond@icloud.com>
2022-03-21 17:11:22 -0700

    Completed Exercise

--- src/index.js
+++ src/index.js
@@ -3,24 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Button = (props) => {
-  // your code here
+  const {resetButton} = props;
+  return (
+    <button onClick={resetButton}>Reset</button>
+  );
 };
 
 const Application = () => {
 
-  // your code here
+  const [name, setName] = useState('');
 
   const reset = () => {
-    console.log("reset");
-    // your code here
+    setName('');
   };
 
   return (
     <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input
+        value={name}
+        onChange={(event)=>setName(event.target.value)}
+        placeholder='Type Your Name' />
+      <Button resetButton={reset} />
+      <h1>{name ? 'Hello ' : ''}{name}</h1>
     </main>
   );
 };
+
 
 ReactDOM.render(<Application />, document.getElementById("root"));
